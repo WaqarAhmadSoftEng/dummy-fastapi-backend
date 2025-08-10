98421107139ffe724a4c47038881a7652f8ee1ae
WaqarAhmadSoftEng <waqarahmad.softeng@gmail.com>
2025-08-10 12:50:01 +0500

    Add New Dummy Records

--- main.py
+++ main.py
@@ -70,7 +70,14 @@ class Attendance(BaseModel):
 # Dummy data
 attendance_data = [
     {"date": "08-08-2025", "student_id": "910", "name": "waqar", "status": "Present"},
+    {"date": "08-07-2025", "student_id": "910", "name": "waqar", "status": "Absent"},
+    {"date": "08-06-2025", "student_id": "910", "name": "waqar", "status": "Absent"},
+    {"date": "08-05-2025", "student_id": "910", "name": "waqar", "status": "Present"},
+    {"date": "08-04-2025", "student_id": "910", "name": "waqar", "status": "Present"},
+
     {"date": "08-08-2025", "student_id": "7767", "name": "waqas", "status": "Present"},
+    {"date": "08-07-2025", "student_id": "7767", "name": "waqas", "status": "Absent"},
+    {"date": "08-06-2025", "student_id": "7767", "name": "waqas", "status": "Present"},
 ]
 
 
